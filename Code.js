42368ecce84c813a463797ceead637c7aafeb428
Thomas M. Hinkle <tmhinkle@gmail.com>
2015-10-18 23:10:54 -0400

    Complete first draft: added basic functions to handle removing/adding files.

--- Code.js
+++ Code.js
@@ -48,8 +48,63 @@ function removeParent (child, parent) {
   Logger.log('Got call to remove parent '+parent+' from child '+child);
   parentFolder = DriveApp.getFolderById(parent);
   childFolder = DriveApp.getFolderById(child);
-  parentFolder.removeFolder(childFolder);
-  return [child,parent,true]
+  // make sure we don't remove the last parent
+  // we don't want to create any orphans
+  var allParentsIterator = childFolder.getParents()
+  allParentsIterator.next() // First parent...
+  if (allParentsIterator.hasNext()) {
+    // As long as there is a second parent, we are happy...
+    parentFolder.removeFolder(childFolder);
+    return [child,parent,true]
+  }
+  else {
+    Logger.log('Refusing to orphan '+child+' by removing parent '+parent)
+    throw "Refusing to Orphan "+childFolder.getName()+" by removing last parent, "+parentFolder.getName();
+  }
+}
+
+function testRemoveParent () {
+  f = DriveApp.createFolder('Test Folder');
+  f2parents = DriveApp.createFolder('Test Folder 2');
+  f.addFolder(f2parents);
+  // Now f2parents is in Root + f
+  // It should be possible to remove f from root
+  removeParent(f.getId(),DriveApp.getRootFolder().getId());
+  // It should *not* be possible to remove f2 from root
+  // because that is its only home :(
+  try {
+    removeParent(f2.getId(),DriveApp.getRootFolder().getId());
+  }
+  catch (err) {
+    Logger.log('Successfully threw error trying to orphan file.')
+    Logger.log('Error',err)
+  }
+  finally {
+    f.setTrashed(true);
+    f2.setTrashed(true);
+  }
+}
+
+
+function removeFromRoot (child) {
+  root = DriveApp.getRootFolder().getId()
+  return removeParent(child,root)
+}
+
+function addMultipleToRoot (children) {
+  return addMultipleToParent(children,DriveApp.getRootFolder().getId())
+}
+
+function addMultipleToParent (children, parent) {
+  childArray = [];
+  parentFolder = DriveApp.getFolderById(parent)
+  for (i=0; i<children.length; i++) {
+    child = DriveApp.getFolderById(children[i]);
+    Logger.log('Got folder '+child+' for ID '+children[i])
+    parentFolder.addFolder(child);
+    childArray.push([child.getId(),child.getName(),child.getUrl()])
+  }
+  return [childArray,[parentFolder.getId(),parentFolder.getName(),parentFolder.getUrl()]]
 }
 
 
@@ -77,4 +132,9 @@ function getMyFolders () {
       Logger.log('Folder '+name+' '+id);
     }}
   Logger.log('Done iterating through folders');
+}
+
+function getOAuthToken() {
+  DriveApp.getRootFolder();
+  return ScriptApp.getOAuthToken();
 }
